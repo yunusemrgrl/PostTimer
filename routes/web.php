da5976a524a53212bea4a1d477b0e9e18f9ae4a7
Yunus Emre GÜRLÜ <yunusemregurlu@gmail.com>
2026-08-21 23:23:04 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,24 +31,11 @@ Route::get('/media/{media:name}/video', [MediaThumbnailController::class, 'video
     ->name('media.video');
 
 Route::get('/debug/instagram-story', function () {
-    $post = InstagramPost::query()
+    $count = InstagramPost::query()
         ->where('media_product_type', InstagramPost::PRODUCT_TYPE_STORY)
-        ->latest('id')
-        ->firstOrFail();
-
-    $url = $post->media_url;
-
-    $response = Http::timeout(30)
-        ->connectTimeout(10)
-        ->get($url);
+        ->count();
 
     return response()->json([
-        'post_id' => $post->id,
-        'status' => $response->status(),
-        'url' => $url,
-        'content_type' => $response->header('Content-Type'),
-        'content_length' => $response->header('Content-Length'),
-        'bytes_received' => strlen($response->body()),
-        'first_32_bytes_hex' => bin2hex(substr($response->body(), 0, 32)),
+        'story_count' => $count,
     ]);
 });
